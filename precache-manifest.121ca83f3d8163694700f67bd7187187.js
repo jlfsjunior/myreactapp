86887d1ea0c6ac9059ddaad6bf2024a9bdfccdb6
jlfsjunior <jlfsjunior@gmail.com>
2020-06-10 20:41:32 +0200

    Updates

---
+++ precache-manifest.121ca83f3d8163694700f67bd7187187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b39f630b014d1ebfdcdfb7258257d8",
+    "url": "/jlfsjunior/myreactapp/index.html"
+  },
+  {
+    "revision": "5faed9601aa0eaa2533f",
+    "url": "/jlfsjunior/myreactapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/jlfsjunior/myreactapp/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jlfsjunior/myreactapp/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5faed9601aa0eaa2533f",
+    "url": "/jlfsjunior/myreactapp/static/js/main.4bce62ff.chunk.js"
+  },
+  {
+    "revision": "c3e4b90b5a5efe110385",
+    "url": "/jlfsjunior/myreactapp/static/js/runtime-main.1e7ad930.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jlfsjunior/myreactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
